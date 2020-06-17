bff23c5db13264b00688a0a5866ea702233d6067
Anton Vasiliev <aionbase@gmail.com>
2020-06-17 23:23:49 +0100

    Implement pilot test for Mongo Adapter

---
+++ tests/functional/Adapter/MongoCest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Incubator\Session\Tests\Functional\Adapter;
+
+use MongoDB\Client;
+use Phalcon\Incubator\Session\Adapter\Mongo;
+
+final class MongoCest
+{
+    public function testWrite(\FunctionalTester $I): void
+    {
+        $client = new Client('mongodb+srv://root:example@127.0.0.1/test?retryWrites=true&w=majority');
+
+        $mongo = new Mongo($client->test->session_data);
+
+        $I->assertTrue($mongo->write('session-id', 'data'));
+    }
+}
