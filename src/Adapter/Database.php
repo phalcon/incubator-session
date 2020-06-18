1576e96f19cf63f01ec1681ff6ef8b0cb971cc89
Anton Vasiliev <aionbase@gmail.com>
2020-06-18 11:42:33 +0100

    Suppress psalm error InvalidScalarArgument

--- src/Adapter/Database.php
+++ src/Adapter/Database.php
@@ -79,6 +79,9 @@ class Database extends AbstractAdapter
         $this->tableName = $tableName;
         $this->prepareColumns($columns);
 
+        /**
+         * @psalm-suppress InvalidScalarArgument
+         */
         session_set_save_handler(
             [$this, 'open'],
             [$this, 'close'],
