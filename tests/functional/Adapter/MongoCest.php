689f9d6581af5375da6cb54d02dc72297fe35e31
Rafael White <rwhite@mctekk.com>
2022-01-25 15:56:15 -0400

    upgrade package to phalcon5

--- tests/functional/Adapter/MongoCest.php
+++ tests/functional/Adapter/MongoCest.php
@@ -16,30 +16,39 @@ namespace Phalcon\Incubator\Session\Tests\Functional\Adapter;
 use FunctionalTester;
 use MongoDB\Client;
 use Phalcon\Incubator\Session\Adapter\Mongo;
+use Dotenv\Dotenv;
 
 final class MongoCest
 {
+    private $mongoClient;
+
+    public function __construct()
+    {
+        $dotenv = new Dotenv(__DIR__ . "/../../_ci/");
+        $dotenv->load();
+
+        $mongoHost = getenv('DATA_MONGO_HOST');
+        $mongoPort = getenv('DATA_MONGO_PORT');
+        $mongoUser = getenv('DATA_MONGO_USER');
+        $mongoPass = getenv('DATA_MONGO_PASS');
+        $this->mongoClient =  new Client("mongodb://$mongoUser:$mongoPass@$mongoHost:$mongoPort");
+    }
+
     public function open(FunctionalTester $I): void
     {
-        $client = new Client('mongodb://127.0.0.1:27017');
-        $mongo = new Mongo($client->test->session_data);
-
+        $mongo = new Mongo($this->mongoClient->test->session_data);
         $I->assertTrue($mongo->open('', 'session-name'));
     }
 
     public function testReadEmpty(FunctionalTester $I): void
     {
-        $client = new Client('mongodb://127.0.0.1:27017');
-        $mongo = new Mongo($client->test->session_data);
-
+        $mongo = new Mongo($this->mongoClient->test->session_data);
         $I->assertSame('', $mongo->read('session-id'));
     }
 
     public function testWriteNew(FunctionalTester $I): void
     {
-        $client = new Client('mongodb://127.0.0.1:27017');
-        $mongo = new Mongo($client->test->session_data);
-
+        $mongo = new Mongo($this->mongoClient->test->session_data);
         $I->assertTrue($mongo->write('session-id', 'data'));
     }
 
@@ -48,9 +57,7 @@ final class MongoCest
         $sessionId = bin2hex(random_bytes(32));
         $sessionData = 'value';
 
-        $client = new Client('mongodb://127.0.0.1:27017');
-        $mongo = new Mongo($client->test->session_data);
-
+        $mongo = new Mongo($this->mongoClient->test->session_data);
         $mongo->open(codecept_output_dir(), $sessionId);
 
         $I->assertTrue($mongo->write($sessionId, $sessionData));
@@ -65,9 +72,7 @@ final class MongoCest
         $sessionData = 'value';
         $newSessionData = 'new-value';
 
-        $client = new Client('mongodb://127.0.0.1:27017');
-        $mongo = new Mongo($client->test->session_data);
-
+        $mongo = new Mongo($this->mongoClient->test->session_data);
         $mongo->open(codecept_output_dir(), $sessionid);
 
         $I->assertTrue($mongo->write($sessionid, $sessionData));
